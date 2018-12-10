cc3b7fe121ea7cc8b9dc0ba6a1968ac2fa0da0bd
RuelOquindo <ruel@fivefish.net>
2018-12-09 22:17:43 -0600

    Use bitwise operations

--- main.cpp
+++ main.cpp
@@ -1,37 +1,31 @@
 #include "mbed.h"
-#include <stdio.h>
-#include <iostream>
-
 using namespace std;
 
-#define DISPLAY_DELAY 0.0012   // seconds
-
-typedef struct SevenSegment
-{
-  int SevenSegmentMap[8];
-};
-
-static const SevenSegment Digits[] = {
-// a, b, c, d, e, f, g, decimal
-  {1, 1, 1, 1, 1, 1, 0, 0},    // 0
-  {0, 1, 1, 0, 0, 0, 0, 0},    // 1
-  {1, 1, 0, 1, 1, 0, 1, 0},    // 2
-  {1, 1, 1, 1, 0, 0, 1, 0},    // 3
-  {0, 1, 1, 0, 0, 1, 1, 0},    // 4
-  {1, 0, 1, 1, 0, 1, 1, 0},    // 5
-  {1, 0, 1, 1, 1, 1, 1, 0},    // 6
-  {1, 1, 1, 0, 0, 0, 0, 0},    // 7
-  {1, 1, 1, 1, 1, 1, 1, 0},    // 8
-  {1, 1, 1, 1, 0, 1, 1, 0},    // 9
-  {1, 1, 1, 0, 1, 1, 1, 0},    // A (10)
-  {0, 0, 1, 1, 1, 1, 1, 0},    // B (11)
-  {1, 0, 0, 1, 1, 1, 0, 0},    // C (12)
-  {0, 1, 1, 1, 1, 0, 1, 0},    // D (13)
-  {1, 0, 0, 1, 1, 1, 1, 0},    // E (14)
-  {1, 0, 0, 0, 1, 1, 1, 0}     // F (15)
-};
-
+// serial comms for debugging
+Serial pc(USBTX, USBRX);  
 DigitalOut led(PG_13);    // just for testing... that the dev board is alive
+
+#define DISPLAY_DELAY 0.0002   // seconds
+
+static const int Digits[] = {
+  //abcdefgp    // 7-segment display + decimal point
+  0b11111100,   // 0
+  0b01100000,   // 1
+  0b11011010,   // 2
+  0b11110010,   // 3
+  0b01100110,   // 4
+  0b10110110,   // 5
+  0b10111110,   // 6
+  0b11100000,   // 7
+  0b11111110,   // 8
+  0b11110110,   // 9
+  0b11101110,   // A (10)
+  0b00111110,   // B (11)
+  0b10011100,   // C (12)
+  0b01111010,   // D (13)
+  0b10011110,   // E (14)
+  0b10001110    // F (15)
+};
 
 // select which digit to light up 
 DigitalOut Digit1(PG_5); // anode for Digit1 (leftmost) set to HIGH to activate Digit
@@ -48,10 +42,7 @@ DigitalOut SegmentF(PD_5);
 DigitalOut SegmentG(PD_6);  // middle segment
 DigitalOut SegmentP(PD_7);  // decimal point 
 
-// serial comms for debugging
-Serial pc(USBTX, USBRX);  
-
-void Display_Init(){
+void Display_Clear(){
   // reset all pins, clear display
   SegmentA = !0;
   SegmentB = !0;
@@ -68,14 +59,15 @@ void Display_Init(){
 
 void Display_Digit(int DigitPosition, int Number){
     // we need to invert logic because we're using a common anode display
-    SegmentA = !Digits[Number].SevenSegmentMap[0];
-    SegmentB = !Digits[Number].SevenSegmentMap[1];
-    SegmentC = !Digits[Number].SevenSegmentMap[2];
-    SegmentD = !Digits[Number].SevenSegmentMap[3];
-    SegmentE = !Digits[Number].SevenSegmentMap[4];
-    SegmentF = !Digits[Number].SevenSegmentMap[5];
-    SegmentG = !Digits[Number].SevenSegmentMap[6];
-    SegmentP = !Digits[Number].SevenSegmentMap[7];
+    SegmentA = !((Digits[Number] & 0b10000000) >> 7);
+    SegmentB = !((Digits[Number] & 0b01000000) >> 6);
+    SegmentC = !((Digits[Number] & 0b00100000) >> 5);
+    SegmentD = !((Digits[Number] & 0b00010000) >> 4);
+    SegmentE = !((Digits[Number] & 0b00001000) >> 3);
+    SegmentF = !((Digits[Number] & 0b00000100) >> 2);
+    SegmentG = !((Digits[Number] & 0b00000010) >> 1);
+    SegmentP = !((Digits[Number] & 0b00000001) >> 0);
+
     // we need to clear out the other digits before displaying the new number. 
     // otherwise, the same number will be displayed in all the digits. 
     switch (DigitPosition) {
@@ -104,15 +96,15 @@ void Display_Number(int Number){
   if (Number > 99){
     hundreds = Number / 100;
     Display_Digit(1, hundreds);
-    wait(0.004);    // if delay is too short, LED will be dim. If too long, we'll have flickering
+    wait(0.003);    // if delay is too short, LED will be dim. If too long, we'll have flickering
 
     tens = (Number % 100) / 10;
     Display_Digit(2, tens);
-    wait(0.001);
+    wait(0.0001);
 
     ones = (Number % 100) % 10;
     Display_Digit(3, ones);
-    wait(0.0009);
+    wait(0.0001);
   }
 
   if (Number > 9){  
@@ -134,7 +126,7 @@ int main() {
   // set usb serial
   pc.baud(115200);
 
-  Display_Init();
+  Display_Clear();
 
   while(true){
     for (int i=0; i<1000; i++){
